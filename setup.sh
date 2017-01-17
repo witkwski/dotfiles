2dea160dcb903444d05c162d2827a08f4cb57b63
witkowski <antnwtkwsk@gmail.com>
2017-01-17 10:16:55 +0200

    fix zsh path

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# TODOS:
-# colors and prompts
-# hardcoded file names
-
+# TODO: make it a little prettier
 print_header() {
   printf '%*s\n' $(tput cols) '' | tr ' ' _
   printf "\n$1\n"
@@ -14,18 +11,15 @@ do_install() {
   sudo apt-get install $1
 }
 
-declare -a packages=('zsh' 'zsh-antigen' 'tmux' 'colortest')
+declare -a packages=(
+  'zsh'
+  'zsh-antigen'
+  'tmux'
+  'colortest'
+)
 
 # authenticate user
 sudo -v
-
-# backup
-print_header 'cloning existed dots into ~/old_dots_backup'
-mkdir ~/old_dots_backup
-cp ~/.bashrc       ~/old_dots_backup
-cp ~/.bash_profile ~/old_dots_backup
-cp ~/.tmux.conf    ~/old_dots_backup
-cp ~/.zshrc        ~/old_dots_backup
 
 # packages
 print_header 'installing packages'
@@ -38,8 +32,8 @@ done
 
 # change shell to zsh
 print_header 'changing shell'
-sudo chsh -s /bin/zsh
+chsh -s $(which zsh)
 
 # symlink dots
-print_header 'building symlinks'
+print_header 'cloning existing dots into ~/old_dots_backup and building symlinks'
 ./symlink-setup.sh
--- symlink-setup.sh
+++ symlink-setup.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+# TODO: get rid of hardcoded filenames
+
+# backup
+mkdir ~/old_dots_backup
+cp ~/.bashrc       ~/old_dots_backup
+cp ~/.bash_profile ~/old_dots_backup
+cp ~/.tmux.conf    ~/old_dots_backup
+cp ~/.zshrc        ~/old_dots_backup
+
+# symlinks
 ln -fs "$(pwd)/.bash_profile" ~
 ln -fs "$(pwd)/.bashrc" ~
 ln -fs "$(pwd)/.tmux.conf" ~
